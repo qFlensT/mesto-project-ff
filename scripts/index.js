855d5f618543b7a742bdc37b81ccd9001d8eaa58
Evgeniy Bondarenko <zhekos1123@ya.ru>
2023-11-23 15:08:47 +0300

    added alt

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
-// @todo: Темплейт карточки
+const createCard = (cardData, removeCallback) => {
+  const link = cardData.link || "https://source.unsplash.com/random";
+  const name = cardData.name || "Нет данных";
+  const alt =
+    cardData.link && cardData.name ? name : "Не удалось загрузить изображение";
 
-// @todo: DOM узлы
+  const cardTemplate = document.querySelector("#card-template").content;
+  const card = cardTemplate.querySelector(".card").cloneNode(true);
 
-// @todo: Функция создания карточки
+  const cardImage = card.querySelector(".card__image");
+  cardImage.src = link;
+  cardImage.alt = alt;
 
-// @todo: Функция удаления карточки
+  card
+    .querySelector(".card__delete-button")
+    .addEventListener("click", removeCallback);
 
-// @todo: Вывести карточки на страницу
+  card.querySelector(".card__title").textContent = name;
+
+  return card;
+};
+
+const removeCallback = (event) => {
+  event.target.parentElement.remove();
+};
+
+const addCard = (card) => {
+  const placesList = document.querySelector(".places__list");
+
+  placesList.append(card);
+};
+
+initialCards.forEach((cardData) => {
+  addCard(createCard(cardData, removeCallback));
+});
